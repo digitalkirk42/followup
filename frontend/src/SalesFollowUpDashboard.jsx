9aaf901ee14e6665f187345e312e10a46a3daa0b
digitalkirk42 <mike.kirk@foghlamtech.com>
2025-04-10 14:55:36 -0500

    Update SalesFollowUpDashboard.jsx

--- frontend/src/SalesFollowUpDashboard.jsx
+++ frontend/src/SalesFollowUpDashboard.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Card } from "../components/ui/card";
+import { Card } from "../components/ui/card2";
 import { Button } from "../components/ui/button";
 import { Textarea } from "../components/ui/textarea";
 import { Input } from "../components/ui/input";
