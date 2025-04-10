0272affcc78284239f514c3ce1e606a41b20a718
digitalkirk42 <mike.kirk@foghlamtech.com>
2025-04-10 15:58:09 -0500

    Update tabs.jsx

--- frontend/src/components/ui/tabs.jsx
+++ frontend/src/components/ui/tabs.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React from "react";
 
 export function Tabs({ defaultValue, children, className }) {
   const [activeTab, setActiveTab] = useState(defaultValue);
