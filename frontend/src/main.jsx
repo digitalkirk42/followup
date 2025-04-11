69cf414061058d2dfb0f188b85e9b8cec9d9c7e0
digitalkirk42 <mike.kirk@foghlamtech.com>
2025-04-11 09:19:08 -0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // Optional styling file if you want to get fancy
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
